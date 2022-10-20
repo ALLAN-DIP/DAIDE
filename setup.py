e830f23699586a4db4c38d20eb3f3682130f6c88
trigaten <sanderman5@hotmail.com>
2022-10-20 09:32:21 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="DAIDE",
-    version="0.0.1",
+    version="0.0.2",
     author="Sander Schulhoff",
     author_email="sanderschulhoff@gmail.com",
     description="DAIDE parsing stuff",
